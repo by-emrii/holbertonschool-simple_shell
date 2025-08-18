bda86a213eb3090ccc17183730d54dc9377e1d06
Emily Chew <emilychewsh@gmail.com>
2025-08-18 04:17:59 +0000

    main.c

---
+++ simple-shell-test/main.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+
+/**
+ * main - entry point
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			printf("($) ");
+			fflush(stdout); /* makes sure it shows immediately */
+		}
+
+		read = getline(&line, &len, stdin);
+		if (read == -1) /* EOF */
+		{
+			free(line);
+			printf("\n"); /* so prompt looks neat */
+			exit(0);
+		}
+		printf("You typed: %s", line);
+	}
+	free(line);
+	return (0);
+}
