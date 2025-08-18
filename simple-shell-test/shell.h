95006763c56e81f0fd7c9cb76c4b80ec7ab43248
Emily Chew <emilychewsh@gmail.com>
2025-08-18 03:24:50 +0000

    init

---
+++ simple-shell-test/shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+/* global variable */
+extern char **environ;
+
+/* prototypes */
+
+#endif
